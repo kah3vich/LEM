14aeb237c134117e69b442d3352b1407a1ea606c
kah3vich <70528062+kah3vich@users.noreply.github.com>
2021-12-13 14:50:35 +0300

    file

--- #src/js/company/company.js
+++ #src/js/company/company.js
@@ -9,12 +9,24 @@ var companySecretSlider = new Swiper(".companySecretSlider", {
     keyboard: true,
     slidesPerView:5,
     breakpoints: {
-        1: {
+        1400: {
             slidesPerView: 5,
             spaceBetweenSlides: 0
         },
+        950: {
+            slidesPerView: 4,
+            spaceBetweenSlides: 0
+        },
+        720: {
+            slidesPerView: 3,
+            spaceBetweenSlides: 0
+        },
+        600: {
+            slidesPerView: 2,
+            spaceBetweenSlides: 0
+        },
         0: {
-            slidesPerView: 5,
+            slidesPerView: 1,
             spaceBetweenSlides: 0
         }
     }
@@ -33,12 +45,47 @@ var companySliders = new Swiper(".companySliders", {
     },
     keyboard: true,
     breakpoints: {
-        1: {
+        1255: {
             slidesPerView: 3,
             spaceBetweenSlides: 40
         },
-        0: {
+        850: {
             slidesPerView: 3,
+            spaceBetweenSlides: 40
+        },
+        550: {
+            slidesPerView: 2,
+            spaceBetweenSlides: 40
+        },
+        0: {
+            slidesPerView: 1,
+            spaceBetweenSlides: 40
+        }
+    }
+});
+
+
+var companyCareerSlider = new Swiper(".companyCareerSlider", {
+    pagination: {
+        el: ".comp-career__content-slider .swiper-controls .swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+    breakpoints: {
+        1050: {
+            slidesPerView: 4,
+            spaceBetweenSlides: 0
+        },
+        900: {
+            slidesPerView: 3,
+            spaceBetweenSlides: 0
+        },
+        600: {
+            slidesPerView: 2,
+            spaceBetweenSlides: 0
+        },
+        0: {
+            slidesPerView: 1,
             spaceBetweenSlides: 0
         }
     }
