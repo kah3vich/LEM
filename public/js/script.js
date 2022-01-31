c4c30f1e4d858a98ca4b587b09ee6c073d3f1961
kah3vich <nik.b20@mail.ru>
2022-01-31 11:02:21 +0300

    file

--- public/js/script.js
+++ public/js/script.js
@@ -753,101 +753,95 @@ $('#newProcessTabs-4').on('click', function() {
     $('#newProcessTabsList-2').addClass('display-n')
 })
 
-var companySecretSlider = new Swiper(".companySecretSlider", {
-    navigation: {
-        nextEl: ".comp-secret__slider-block .swiper-controls .swiper-button-next",
-        prevEl: ".comp-secret__slider-block .swiper-controls .swiper-button-prev",
-    },
-    pagination: {
-        el: ".comp-secret__slider-block .swiper-controls .swiper-pagination",
-    },
-    keyboard: true,
-    slidesPerView:5,
-    breakpoints: {
-        1400: {
-            slidesPerView: 5,
-            spaceBetweenSlides: 0
-        },
-        950: {
-            slidesPerView: 4,
-            spaceBetweenSlides: 0
-        },
-        720: {
-            slidesPerView: 3,
-            spaceBetweenSlides: 0
-        },
-        600: {
-            slidesPerView: 2,
-            spaceBetweenSlides: 0
-        },
-        0: {
-            slidesPerView: 2,
-            spaceBetweenSlides: 26
-        }
-    }
-});
+var companySecretSlider = new Swiper('.companySecretSlider', {
+	navigation: {
+		nextEl: '.comp-secret__slider-block .swiper-controls .swiper-button-next',
+		prevEl: '.comp-secret__slider-block .swiper-controls .swiper-button-prev',
+	},
+	pagination: {
+		el: '.comp-secret__slider-block .swiper-controls .swiper-pagination',
+	},
+	keyboard: true,
+	slidesPerView: 5,
+	breakpoints: {
+		1400: {
+			slidesPerView: 5,
+			spaceBetweenSlides: 0,
+		},
+		950: {
+			slidesPerView: 4,
+			spaceBetweenSlides: 0,
+		},
+		720: {
+			slidesPerView: 3,
+			spaceBetweenSlides: 0,
+		},
+		600: {
+			slidesPerView: 2,
+			spaceBetweenSlides: 0,
+		},
+		0: {
+			slidesPerView: 2,
+			spaceBetweenSlides: 26,
+		},
+	},
+})
 
+var companySliders = new Swiper('.companySliders', {
+	loop: true,
+	navigation: {
+		nextEl: '.comp-slider__block-controls-arrow-next',
+		prevEl: '.comp-slider__block-controls-arrow-prev',
+	},
+	pagination: {
+		el: '.comp-slider__block-sliders .swiper-controls .swiper-pagination',
+	},
+	keyboard: true,
+	breakpoints: {
+		1255: {
+			slidesPerView: 3,
+			spaceBetweenSlides: 40,
+		},
+		850: {
+			slidesPerView: 3,
+			spaceBetweenSlides: 40,
+		},
+		550: {
+			slidesPerView: 2,
+			spaceBetweenSlides: 40,
+		},
+		0: {
+			slidesPerView: 1,
+			spaceBetweenSlides: 40,
+		},
+	},
+})
 
-
-var companySliders = new Swiper(".companySliders", {
-    loop: true,
-    navigation: {
-        nextEl: ".comp-slider__block-controls-arrow-next",
-        prevEl: ".comp-slider__block-controls-arrow-prev",
-    },
-    pagination: {
-        el: ".comp-slider__block-sliders .swiper-controls .swiper-pagination",
-    },
-    keyboard: true,
-    breakpoints: {
-        1255: {
-            slidesPerView: 3,
-            spaceBetweenSlides: 40
-        },
-        850: {
-            slidesPerView: 3,
-            spaceBetweenSlides: 40
-        },
-        550: {
-            slidesPerView: 2,
-            spaceBetweenSlides: 40
-        },
-        0: {
-            slidesPerView: 1,
-            spaceBetweenSlides: 40
-        }
-    }
-});
-
-
-var companyCareerSlider = new Swiper(".companyCareerSlider", {
-    pagination: {
-        el: ".comp-career__content-slider .swiper-controls .swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
-    breakpoints: {
-        1050: {
-            slidesPerView: 4,
-            spaceBetweenSlides: 0
-        },
-        900: {
-            slidesPerView: 3,
-            spaceBetweenSlides: 0
-        },
-        600: {
-            slidesPerView: 2,
-            spaceBetweenSlides: 0
-        },
-        0: {
-            slidesPerView: 1,
-            spaceBetweenSlides: 0
-        }
-    }
-});
-
-
-
+var companyCareerSlider = new Swiper('.companyCareerSlider', {
+	pagination: {
+		el: '.comp-career__content-slider .swiper-controls .swiper-pagination',
+	},
+	mousewheel: true,
+	keyboard: true,
+	breakpoints: {
+		1050: {
+			slidesPerView: 4,
+			spaceBetweenSlides: 0,
+		},
+		900: {
+			slidesPerView: 3,
+			spaceBetweenSlides: 0,
+		},
+		600: {
+			slidesPerView: 2,
+			spaceBetweenSlides: 0,
+		},
+		0: {
+			slidesPerView: 1,
+			spaceBetweenSlides: 0,
+		},
+	},
+})
 
 let companyCountCollection = $('.comp-own__block-items-img').length
 
@@ -877,26 +871,23 @@ $('.modelCompanyCollection__close').on('click', () => {
 	$('body').css('overflow', 'visible')
 })
 
-$(() => {
-	var selected = $('.comp-own__block-items-wrapper img')
-		.map(function () {
-			if ($(this).attr('src')) return $(this).attr('src')
-		})
-		.get()
-	$.each(selected, function (index, value) {
-		let newElementBlock = document.createElement('div')
-		newElementBlock.classList.add('swiper-slide')
-		newElementBlock.innerHTML = `
-		<div class="modelCompanyCollection__wrapper-items">
-			<img src="${value}" alt="img" />
-		</div>
-		`
-		document.getElementById('companyCollection').appendChild(newElementBlock)
-	})
-})
-
-
-
+// $(() => {
+// 	var selected = $('.comp-own__block-items-wrapper img')
+// 		.map(function () {
+// 			if ($(this).attr('src')) return $(this).attr('src')
+// 		})
+// 		.get()
+// 	$.each(selected, function (index, value) {
+// 		let newElementBlock = document.createElement('div')
+// 		newElementBlock.classList.add('swiper-slide')
+// 		newElementBlock.innerHTML = `
+// 		<div class="modelCompanyCollection__wrapper-items">
+// 			<img src="${value}" alt="img" />
+// 		</div>
+// 		`
+// 		document.getElementById('companyCollection').appendChild(newElementBlock)
+// 	})
+// })
 
 let companyCountTex = $('.comp-own__blocks-items-img').length
 
@@ -926,23 +917,24 @@ $('.modelCompanyTex__close').on('click', () => {
 	$('body').css('overflow', 'visible')
 })
 
-$(() => {
-	var selected = $('.comp-own__blocks-items-wrapper img')
-		.map(function () {
-			if ($(this).attr('src')) return $(this).attr('src')
-		})
-		.get()
-	$.each(selected, function (index, value) {
-		let newElementBlock = document.createElement('div')
-		newElementBlock.classList.add('swiper-slide')
-		newElementBlock.innerHTML = `
-		<div class="modelCompanyTex__wrapper-items">
-			<img src="${value}" alt="img" />
-		</div>
-		`
-		document.getElementById('companyTex').appendChild(newElementBlock)
-	})
-})
+// $(() => {
+// 	var selected = $('.comp-own__blocks-items-wrapper img')
+// 		.map(function () {
+// 			if ($(this).attr('src')) return $(this).attr('src')
+// 		})
+// 		.get()
+// 	$.each(selected, function (index, value) {
+// 		let newElementBlock = document.createElement('div')
+// 		newElementBlock.classList.add('swiper-slide')
+// 		newElementBlock.innerHTML = `
+// 		<div class="modelCompanyTex__wrapper-items">
+// 			<img src="${value}" alt="img" />
+// 		</div>
+// 		`
+// 		document.getElementById('companyTex').appendChild(newElementBlock)
+// 	})
+// })
+
 
 let activeContentObject = 0
 let elementContentCountObject = $('.obj-content').length;
